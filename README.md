483b5e45a59db19aecac9a72b53560386302d629
Taylor Ren <taylor.ren@gmail.com>
2013-11-04 20:52:30 +0800

    Using Dart/Polymer to re-write the program

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
-AnimalGuess
-===========
+###README
 
-A legacy Animal Guess game
+Animal Guess is a legacy AI game which can be traced back to the epoch of BASIC. 
+
+The game tries to guess the animal the user has in mind by asking YES/NO questions. It will prompt more questions until it reaches the guess it has in "mind". 
+
+If the guess if correct (assuming the user does not change his mind during the game process), it will be happy; if not, the game will seek the user's help by asking the user to provide another YES/NO question, the new animal the user has in mind, and the Y/N answer to that question to distinguish the two animals. 
+
+As time goes by, the game will become "smarter" as it will ask more questions before it prompts the guess. 
+
+This program is developed with PHP (Symfony 2 framework) and Dart. PHP is used as the RESTful API provider (the server) and Dart used to mimic a frontend. 
---
+++ web/animalguessclass.dart
@@ -0,0 +1,58 @@
+import 'package:polymer/polymer.dart';
+import 'dart:html';
+import 'dart:convert';
+
+
+/**
+ * A Polymer click counter element.
+ */
+@CustomTag('animal-guess')
+class AnimalGuess extends PolymerElement {
+  @published bool gameinprogress=false;
+  @published String question='';
+  @published int qid=1;
+  int yBranch;
+  int nBranch;
+
+  AnimalGuess.created() : super.created() {
+  }
+
+  void newGame() {
+    gameinprogress=true;
+    
+    getQuestionById(qid);
+  }
+  
+  void getQuestionById(qid)
+  {
+    var path='http://rsywx/app_dev.php/animal/getQuestionById/$qid';
+    var req=new HttpRequest();
+    req..open('GET', path)
+      ..onLoadEnd.listen((e)=>requestComplete(req))
+      ..send('');
+  }
+  
+  void YBranch()
+  {
+    getQuestionById(yBranch);
+    qid=yBranch;
+  }
+  
+  void NBranch()
+  {
+    getQuestionById(nBranch);
+    qid=nBranch;
+  }
+  
+  void requestComplete(HttpRequest req)
+  {
+    if (req.status==200)
+    {
+      Map res=JSON.decode(req.responseText);
+      question=res['q'];
+      yBranch=res['y'];
+      nBranch=res['n'];
+    }
+  }
+}
+
